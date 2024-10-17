dc0ba77fead04bfaa3592c0619423ee6f26fde39
몽키해드 <killkimno@hotmail.com>
2024-10-17 17:18:19 +0900

    스코프 업데이트

--- PageNavigation/Assets/Script/Page/DialogueConfirm/DialogueConfirmLifetimeScope.cs
+++ PageNavigation/Assets/Script/Page/DialogueConfirm/DialogueConfirmLifetimeScope.cs
@@ -1,9 +1,19 @@
+using Script.Base.Page.Presenter;
+using UnityEngine;
+using VContainer;
 using VContainer.Unity;
 
 namespace Script.Page.DialogueConfirm
 {
     public class DialogueConfirmLifetimeScope : LifetimeScope
     {
-        
+        [SerializeField]
+        private DialogueConfirmView _view;
+
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.Register<DialogueConfirmPresenter>(Lifetime.Scoped).As<IPresenter>();
+            builder.RegisterComponent<DialogueConfirmView>(_view).AsImplementedInterfaces();
+        }
     }
 }
